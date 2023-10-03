b176b23331de55b0f8a50efab0df909f46041497
Joan Camps <kmps84@gmail.com>
2023-10-03 12:42:35 +0200

    Enunciados de ejercicios hasta #3

--- Reto#0/Reto#0.py
+++ Reto#0/Reto#0.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
-    if ((i % 3) == 0) and ((i % 5) == 0):
+    if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
-    elif (i % 5) == 0:
+    elif i % 5 == 0:
         print("buzz")
-    elif (i % 3) == 0:
+    elif i % 3 == 0:
         print("fizz")
     else:
         print(i)
